1b0fa51d3cae92d6ef37a095e540f01fcbfdeb0d
HoonJae <rlagnswo0505@naver.com>
2022-06-30 09:28:19 +0900

    feed에 이미지파일 한개 추가

--- application/controllers/FeedController.php
+++ application/controllers/FeedController.php
@@ -7,6 +7,7 @@ class FeedController extends Controller
     public function index()
     {
         $this->addAttribute(_JS, ["feed/index"]);
+        $this->addAttribute(_CSS, ["feed/index"]);
         $this->addAttribute(_MAIN, $this->getView("feed/index.php"));
         return "template/t1.php";
 
@@ -71,7 +72,11 @@ class FeedController extends Controller
               "startIdx" => $startIdx,
               "iuser" => getIuser()
             ];
-            return $this->model->selFeedList($param);
+            $list = $this->model->selFeedList($param);
+            foreach($list as $item){
+              $item->imgList = $this->model->selFeedImgList($item);
+            }
+            return $list;
         }
     }
 }
--- static/js/feed/index.js
+++ static/js/feed/index.js
@@ -116,20 +116,46 @@
       divContainer.appendChild(divTop);
 
       const regDtInfo = getDateTimeInfo(item.regdt);
-      divTop.className = 'd-flex flex-row ps-3 pe-3';
+      divTop.className = 'd-flex flex-column ps-3 pe-3';
       const writerImg = `<img src='/static/img/profile/${item.iuser}/${item.mainimg}' 
               onerror='this.error=null;this.src="/static/img/profile/defaultProfileImg_100.png"'>`;
 
       divTop.innerHTML = `
-              <div class="d-flex flex-column justify-content-center">${writerImg}</div>
-              <div class="p-3 flex-grow-1">
-                  <div><span class="pointer" onclick="moveToProfile(${item.iuser});">${
-        item.writer
-      }</span> - ${regDtInfo}</div>
-                  <div>${item.location === null ? '' : item.location}</div>
-              </div>
+                <div class="d-flex flex-row justify-content-center align-item-center">
+                <div class="d-flex flex-column justify-content-center ps-3">
+                  <div class="circleimg h40 w40">${writerImg}</div>
+                </div>
+                <div class="p-3 flex-grow-1">
+                  <div>
+                    <span class="pointer" onclick="moveToProfile(${item.iuser});">
+                      ${item.writer}
+                    </span> - ${regDtInfo}
+                  </div>
+                  <div>
+                    ${item.location === null ? '' : item.location}
+                  </div>
+                </div>
+                </div>
           `;
+      const divImgSwiper = document.createElement('div');
+      divContainer.appendChild(divImgSwiper);
+      divImgSwiper.className = 'swiper item_img';
+      divImgSwiper.innerHTML = `
+        <div class="swiper-wrapper"></div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-next"></div>
+      `;
+      const divSwipeWrapper = divImgSwiper.querySelector('.swiper-wrapper');
+      const imgObj = item.imgList[0];
+      const divSwiperSlide = document.createElement('div');
+      divSwipeWrapper.appendChild(divSwiperSlide);
+      divSwiperSlide.classList.add('swiper-slide');
 
+      const img = document.createElement('img');
+      divSwiperSlide.appendChild(img);
+      img.className = 'w614';
+      img.src = `/static/img/feed/${item.ifeed}/${imgObj.img}`;
       return divContainer;
     },
 
